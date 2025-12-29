aef3801218ae4ec2a5ad36689b13c67d2220f695
surinrasu <srs@surindaku.dev>
2025-12-29 11:10:00 +0800

    feat(app): add SwiftPM scaffold

---
+++ PoseKitApp/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "PoseKitApp",
+    platforms: [.iOS(.v17)],
+    products: [
+        .executable(name: "PoseKitApp", targets: ["PoseKitApp"])
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "PoseKitApp",
+            dependencies: []
+        )
+    ]
+)
---
+++ PoseKitApp/Sources/PoseKitApp/App/PoseKitApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct PoseKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootTabView()
+        }
+    }
+}
